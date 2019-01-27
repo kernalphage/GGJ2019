725f3e05ebb3755e4f8ffebaeda1ae8e80d2093b
Matt Dobler <doblerman@gmail.com>
2019-01-26 17:38:04 -0800

    chew toy time

--- Assets/Assets/Scripts/Chew.cs
+++ Assets/Assets/Scripts/Chew.cs
@@ -6,10 +6,14 @@ public class Chew : MonoBehaviour
 {
     public GameObject mouth;
     public GameObject bone;
-
-
+    public Sprite[] anims;
     public string[] sequence;
+    public float[] rotations;
     public int idx;
+    private float currot;
+    private float targetRot;
+    public float rotSpeed;
+
 
     private int score = 0;
 
@@ -37,10 +41,13 @@ public class Chew : MonoBehaviour
     {
         if (Input.GetButtonDown(sequence[idx]))
         {
-            idx = (idx + 1) % sequence.Length;
-          
-        Debug.Log("now press " + idx);
+            mouth.GetComponent<SpriteRenderer>().sprite = anims[idx];
+            targetRot = rotations[idx];
+            idx = (idx + 1) % sequence.Length;          
+            Debug.Log("now press " + idx);
         }
+        currot = Mathf.LerpAngle(currot, targetRot, Time.deltaTime * rotSpeed); ;
+        mouth.transform.rotation = Quaternion.Euler(0, 0, currot);
         //mouth.transform.rotation = Quaternion.Euler(0, 0, Mathf.LerpAngle(mouthMin, mouthMax, mouthT));
         //bone.transform.rotation = Quaternion.Euler(0, 0, Mathf.LerpAngle(boneMin, boneMax, boneT));
     }
