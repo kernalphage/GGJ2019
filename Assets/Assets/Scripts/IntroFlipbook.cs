74e5f3b3fc6e2eec2fd62ee48e366ceb9dba6397
sgaff_Laptop <sgaff29@gmail.com>
2019-01-27 07:41:33 -0800

    Set up intro animatic
    could still use some smoothing, but I want to see if there are other things I should prioritize first

--- Assets/Assets/Scripts/IntroFlipbook.cs
+++ Assets/Assets/Scripts/IntroFlipbook.cs
@@ -1,8 +1,95 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class IntroFlipbook : flipbook
 {
+
+    [SerializeField]
+    private SpriteRenderer specialSprite;
+
+    private bool stopThePresses = false;
+
+    public override void Start()
+    {
+        base.Start();
+
+        if (specialSprite.gameObject.activeInHierarchy && null != specialSprite.gameObject)
+        {
+            //If it's currently active, turn it off.
+            specialSprite.gameObject.SetActive(false);
+        }
+        Debug.Log("Binding");
+        idx = -1;
+    }
+
+
+    ///Need a way so that once we get to the last frame, we hold off on the normal transition that flipbook does; 
+    ///this COULD be done by just setting flipbook to 30 while we wait for the motion to play out :3
+    ///
+    private IEnumerator HandleSpecialIntroLastIndexSprite()
+    {
+        //do this immediately, no delay.
+        stopThePresses = true;
+        idx++;
+        idx = sprites.Count - 1; //grab last sprite
+        reallyFinish = false;
+        setSprite();
+        //This is all crap but it's going to work so eat my shorts.
+        Debug.Log("Shane is a god");
+        yield return new WaitForSeconds(3.0f);
+
+        //Do things after  delay yay..like alpha fading out previous sprite, alpha fading in new image.
+        if (null != specialSprite) //turn on the doggo.
+            specialSprite.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(1.0f);
+        if (null != specialSprite.gameObject.GetComponent<Animator>())
+        {
+            if (null != specialSprite.gameObject.GetComponent<Animator>().runtimeAnimatorController)
+            {
+                specialSprite.gameObject.GetComponent<Animator>().SetBool("initiateTailWag", true);
+
+            }
+        }
+
+        yield return new WaitForSeconds(3.0f);
+        SceneManager.LoadScene("DreamHomeScene");
+        //fliptime = 80;
+
+    }
+
+    public override void Update()
+    {
+        //base.Update();
+        curtime -= Time.deltaTime;
+        if (curtime <= 0)
+        {
+            if (!stopThePresses)
+            {
+                curtime += fliptime;
+                idx++;
+
+            }
+
+            if (idx == sprites.Count)
+            {
+
+                StartCoroutine(HandleSpecialIntroLastIndexSprite());
+                if (onFinished.Length > 0 && reallyFinish) //way to limit the serious ending here.
+                {
+                    transform.SendMessage(onFinished);
+                }
+
+
+
+            }
+
+            idx = idx % sprites.Count;
+            setSprite();
+        }
+    }
 
 }
--- Assets/Assets/Scripts/flipbook.cs
+++ Assets/Assets/Scripts/flipbook.cs
@@ -2,42 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class flipbook : MonoBehaviour {
+public class flipbook : MonoBehaviour
+{
 
     public float fliptime;
-    private float curtime;
+    protected float curtime;
     public List<Sprite> sprites;
     public string onFinished;
-    int idx = 0;
+    protected int idx = 0;
 
-	// Use this for initialization
-	void Start () {
+    public delegate void HandleLastIndexInFlipbook();
+    public static event HandleLastIndexInFlipbook callLastIndex;
+
+    protected bool reallyFinish = true;
+
+    // Use this for initialization
+    public virtual void Start()
+    {
         curtime = fliptime;
-	}
-    void setSprite()
+    }
+    public virtual void setSprite()
     {
         this.GetComponent<SpriteRenderer>().sprite = sprites[idx];
     }
 
-    void StartFlipbook(float time)
+    public virtual void StartFlipbook(float time)
     {
         fliptime = 1;
         curtime = 0;
     }
     // Update is called once per frame
-    void Update () {
+    public virtual void Update()
+    {
         curtime -= Time.deltaTime;
-        if(curtime <= 0)
+        if (curtime <= 0)
         {
             curtime += fliptime;
             idx++;
             if (idx == sprites.Count && onFinished.Length > 0)
             {
+
+
                 transform.SendMessage(onFinished);
+
+
             }
 
             idx = idx % sprites.Count;
             setSprite();
         }
     }
+
+  
+
+
+
 }
